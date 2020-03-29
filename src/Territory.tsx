60338aae70820b8a996b961bfd9debb0828b1bd1
Michael Sealand <michael.sealand@poly.com>
2020-03-28 22:47:07 -0700

    Mouse over stuff

--- src/Territory.tsx
+++ src/Territory.tsx
@@ -14,32 +14,48 @@ export type Territory = {
   units: number;
 };
 
+type TerritoryComponentState = {
+  territory: Territory,
+  hovering: boolean
+}
+
 export const TerritoryComponent = (props: { territory: Territory }) => {
-  const [territory, setTerritory] = useState<Territory>(props.territory);
+  const [territoryState, setTerritoryState] = useState<TerritoryComponentState>({ territory: props.territory, hovering: false });
+  const territory = territoryState.territory;
 
   const handleClick = () => {
     if (territory.colorIdx === undefined) {
       territory.colorIdx = 7;
     }
     territory.units = (territory.units ?? 0) + 1;
-    setTerritory(Object.assign({}, territory));
+    setTerritoryState({ territory, hovering: territoryState.hovering });
   };
+
+  const handleMouseEnter = () => {
+    setTerritoryState({ territory, hovering: true })
+  }
+
+  const handleMouseLeave = () => {
+    setTerritoryState({ territory, hovering: false });
+  }
 
   const units = territory.units ?? 0;
 
   const fillColor = territory.colorIdx ? `hsl(${(territory.colorIdx ?? 0) * 137.508}, 50%, 25%)` : `hsl(0, 0%, 25%)`;
   const strokeColor = territory.colorIdx ? `hsl(${(territory.colorIdx ?? 0) * 137.508}, 100%, 70%)` : `hsl(0, 0%, 70%)`;
 
+  const scale = territoryState.hovering ? 1.5 : 1.0
+
   return (
     <React.Fragment>
-      <Circle x={territory.position.x} y={territory.position.y} radius={9} fill={fillColor} />
-      <Circle x={territory.position.x} y={territory.position.y} radius={9} strokeWidth={2} stroke={strokeColor} />
+      <Circle x={territory.position.x} y={territory.position.y} radius={9} fill={fillColor} scale={{ x: scale, y: scale}} />
+      <Circle x={territory.position.x} y={territory.position.y} radius={9} strokeWidth={2} stroke={strokeColor} scale={{ x: scale, y: scale}} />
       <Text 
         x={territory.position.x - (territory.units < 10 ? 11.5 : 12)} // Being super picky here
         y={territory.position.y - 11.5} 
         width={24} 
         height={24} 
-        fontSize={units < 100 ? (units < 10 ? 11 : 10) : 8} 
+        fontSize={(units < 100 ? (units < 10 ? 11 : 10) : 8) * scale}
         fontStyle="bold"
         fontFamily="source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace"
         fill="white" 
@@ -48,6 +64,8 @@ export const TerritoryComponent = (props: { territory: Territory }) => {
 
         text={`${units}`} 
         onClick={handleClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       />
     </React.Fragment>
   );
