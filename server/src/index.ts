0fa0a0de160e414b7870c416a370fa71f13907da
Michael Sealand <msealand@mac.com>
2020-04-04 13:12:44 -0700

    Persist game data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import { PolyWar } from 'polywar'
-import { Server } from 'boardgame.io/server';
+import { Server, FlatFile } from 'boardgame.io/server';
+import { resolve } from 'path';
 
 const server = Server({
   games: [PolyWar],
+  db: new FlatFile({
+    dir: resolve(__dirname, '..', 'games'),
+    logging: true
+  }),
 });
 
 server.run(8000);
