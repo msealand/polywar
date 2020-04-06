c4cc2bfe3ecfa837c420017dce2f4b0a9b87cac5
Michael Sealand <msealand@mac.com>
2020-04-05 20:28:53 -0700

    Cleanup Room

--- ui/web/src/lobby/Room.tsx
+++ ui/web/src/lobby/Room.tsx
@@ -17,74 +17,108 @@ type RoomProps = {
 };
 
 export const Room = (props: RoomProps) => {
-  const _createSeat = (gameId, player, idx) => {
-    return (<li className="list-group-item" key={gameId + '-player-' + player.id}>
-      <p className="text-monospace mb-0">
-        Slot {idx + 1}: {player.name || '[free]'}
-      </p>
-    </li>);
+  const gameId = props.room.gameID;
+
+  const playerSlot = (player, idx) => {
+    return (
+      <li className="list-group-item" key={gameId + '-player-' + player.id}>
+        <p className="text-monospace mb-0">
+          Slot {idx + 1}: {player.name || '[free]'}
+        </p>
+      </li>
+    );
   };
 
-  const _createButtonJoin = (inst, seatId) => {
-    const disabled = props.isInGame;
-    return (<button key={'button-join-' + inst.gameID} type="button" className="btn btn-sm btn-success ml-1" onClick={() => props.handleJoin(inst.gameName, inst.gameID, '' + seatId)} disabled={disabled}>
-      Join
-    </button>);
+  const joinButton = (game, seatId) => {
+    return (
+      <button 
+        key={'button-join-' + game.gameID} 
+        type="button" className="btn btn-sm btn-success ml-1" 
+        onClick={() => props.handleJoin(game.gameName, game.gameID, '' + seatId)} 
+        disabled={props.isInGame}
+      >
+        Join
+      </button>
+    );
   };
 
-  const _createButtonLeave = inst => (<button key={'button-leave-' + inst.gameID} type="button" className="btn btn-sm btn-danger ml-1" onClick={() => props.handleLeave(inst.gameName, inst.gameID)}>
-    Leave
-  </button>);
+  const leaveButton = (game) => (
+    <button 
+      key={'button-leave-' + game.gameID} 
+      type="button" className="btn btn-sm btn-danger ml-1" 
+      onClick={() => props.handleLeave(game.gameName, game.gameID)}
+    >
+      Leave
+    </button>
+  );
 
-  const _createButtonPlay = (inst, seatId) => (<button key={'button-play-' + inst.gameID} type="button" className="btn btn-sm btn-success ml-1" onClick={() => props.handlePlay(inst.gameName, {
-    gameID: inst.gameID,
-    playerID: '' + seatId,
-    numPlayers: inst.players.length,
-  })}>
-    Play
-  </button>);
+  const playButton = (game, seatId) => (
+    <button 
+      key={'button-play-' + game.gameID} 
+      type="button" className="btn btn-sm btn-success ml-1" 
+      onClick={() => props.handlePlay(game.gameName, {
+        gameID: game.gameID,
+        playerID: '' + seatId,
+        numPlayers: game.players.length,
+      })}
+    >
+      Play
+    </button>
+  );
 
-  const _createButtonSpectate = inst => (<button key={'button-spectate-' + inst.gameID} type="button" className="btn btn-sm btn-primary ml-1" onClick={() => props.handlePlay(inst.gameName, {
-    gameID: inst.gameID,
-    numPlayers: inst.players.length,
-  })}>
-    Spectate
-  </button>);
+  const spectateButton = (game) => (
+    <button 
+      key={'button-spectate-' + game.gameID} 
+      type="button" className="btn btn-sm btn-primary ml-1" 
+      onClick={() => props.handlePlay(game.gameName, {
+        gameID: game.gameID,
+        numPlayers: game.players.length,
+      })}
+    >
+      Spectate
+    </button>
+  );
 
-  const _createInstanceButtons = inst => {
-    const playerSeat = inst.players.find(player => player.name === props.playerName);
-    const freeSeat = inst.players.find(player => !player.name);
+  const roomButtons = (game) => {
+    const playerSeat = game.players.find(player => player.name === props.playerName);
+    const freeSeat = game.players.find(player => !player.name);
+
     if (playerSeat && freeSeat) {
       // already seated: waiting for game to start
-      return _createButtonLeave(inst);
+      return leaveButton(game);
     }
+
     if (freeSeat) {
       // at least 1 seat is available
-      return _createButtonJoin(inst, freeSeat.id);
+      return joinButton(game, freeSeat.id);
     }
+
     // room is full
     if (playerSeat) {
       return (<div>
         {[
-          _createButtonPlay(inst, playerSeat.id),
-          _createButtonLeave(inst),
+          playButton(game, playerSeat.id),
+          leaveButton(game),
         ]}
       </div>);
     }
+
     // allow spectating
-    return _createButtonSpectate(inst);
+    return spectateButton(game);
   };
   
-  return (<div className="container-fluid">
-    <div className="row">
-      <div className="col">
-        <ul className="list-group">
-          {props.room.players.map(_createSeat.bind(null, props.room.gameID))}
-        </ul>
-      </div>
-      <div className="col col-3 text-right">
-        {_createInstanceButtons(props.room)}
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <ul className="list-group">
+            {props.room.players.map(playerSlot)}
+          </ul>
+        </div>
+        <div className="col col-3 text-right">
+          {roomButtons(props.room)}
+        </div>
       </div>
     </div>
-  </div>);
+  );
 };
